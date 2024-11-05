edfad0cffdfea4d3eab3783057cc16fb66211e8a
akzhol0 <ak.akzhol1603@gmail.com>
2024-11-05 12:36:24 +0500

    debug nextjs

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,17 @@
-import LoginComponent from '@/components/auth/LoginComponent';
 import { Metadata } from 'next';
 import React from 'react';
+import dynamic from 'next/dynamic';
 
 export const metadata: Metadata = {
   title: 'Войти в аккаунт',
 };
 
+const ClientOnlyLoginComponents = dynamic(() => import('@/components/auth/LoginComponent'), {
+  ssr: false,
+});
 
 function Login() {
-  return <LoginComponent />;
+  return <ClientOnlyLoginComponents />;
 }
 
 export default Login;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,17 @@
-import RegisterComponent from '@/components/auth/RegisterComponent';
 import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
 import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Регистрация',
 };
 
+const ClientOnlyRegisterComponent = dynamic(() => import('@/components/auth/RegisterComponent'), {
+  ssr: false,
+});
+
 function Register() {
-  return <RegisterComponent />;
+  return <ClientOnlyRegisterComponent />;
 }
 
 export default Register;
