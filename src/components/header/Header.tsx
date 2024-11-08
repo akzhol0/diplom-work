fdea371f371024194b1218cc71999f29456769b0
akzhol0 <ak.akzhol1603@gmail.com>
2024-11-08 20:04:07 +0500

    added brg to move HEY YO

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,32 @@
-'use client';
+"use client";
 
-import React, { useContext, useState } from 'react';
-import Brand from './Brand';
-import NavBar from '../nav-bar/NavBar';
-import BurgerButton from './header-burger/BurgerButton';
-import { contextData } from '../context/context';
+import React, { useContext, useState } from "react";
+import Brand from "./Brand";
+import NavBar from "../nav-bar/NavBar";
+import BurgerButton from "./header-burger/BurgerButton";
+import { contextData } from "../context/context";
 
 function Header() {
   const { burgerMenu, setBurgerMenu } = useContext(contextData);
 
   return (
     <div className="sticky top-0 w-full flex justify-center bg-[#ffffff]">
-      <div className="w-[95%] lg:w-[80%] h-[80px] flex items-center justify-between gap-4">
+      <div className="w-[95%] lg:w-[80%] h-[80px] flex items-center justify-center md:justify-between gap-4">
         <Brand />
         <div className="hidden md:block">
           <NavBar mobileVersion={false} />
         </div>
-        <div className="block md:hidden">
-          <BurgerButton setBurgerMenu={setBurgerMenu} burgerMenu={burgerMenu} />
-          {burgerMenu && (
-            <div
-              className="left-0 top-[80px] absolute w-full flex justify-center items-center min-h-[200px] 
-                          bg-white border-b border-black">
-              <NavBar mobileVersion={true} />
-            </div>
-          )}
-        </div>
+      </div>
+      <div className="flex pt-6 pe-6 md:hidden">
+        <BurgerButton setBurgerMenu={setBurgerMenu} burgerMenu={burgerMenu} />
+        {burgerMenu && (
+          <div
+            className="absolute left-0 top-[80px] w-full flex justify-center items-center min-h-[200px]
+                          bg-white border-b border-black"
+          >
+            <NavBar mobileVersion={true} />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/header/header-burger/BurgerButton.tsx
+++ src/components/header/header-burger/BurgerButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type BurgerButtonProps = {
   burgerMenu: boolean;
@@ -6,10 +6,19 @@ type BurgerButtonProps = {
 };
 
 function BurgerButton({ burgerMenu, setBurgerMenu }: BurgerButtonProps) {
-  return (
+  return burgerMenu ? (
     <div
       onClick={() => setBurgerMenu(!burgerMenu)}
-      className="w-[40px] h-[30px] flex flex-col justify-between cursor-pointer">
+      className="w-[40px] h-[40px] flex flex-col justify-center cursor-pointer"
+    >
+      <span className="absolute w-[40px] h-[3px] bg-black rotate-45 origin-center"></span>
+      <span className="absolute w-[40px] h-[3px] bg-black -rotate-45 origin-center"></span>
+    </div>
+  ) : (
+    <div
+      onClick={() => setBurgerMenu(!burgerMenu)}
+      className="w-[40px] h-[30px] flex flex-col justify-between cursor-pointer"
+    >
       <span className="w-full h-[3px] bg-black"></span>
       <span className="w-full h-[3px] bg-black"></span>
       <span className="w-full h-[3px] bg-black"></span>
