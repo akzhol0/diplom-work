1bec42620c3e303f91babecaf5881a511740151c
akzhol0 <ak.akzhol1603@gmail.com>
2025-03-07 20:10:52 +0500

    goUpButton not working debug

--- src/components/UI/my-buttons/go-up-button/GoUpButton.tsx
+++ src/components/UI/my-buttons/go-up-button/GoUpButton.tsx
@@ -3,7 +3,7 @@ import MyArrowUp from "@/components/UI/icons/my-arrow/MyArrowUp";
 
 const GoUpButton = () => {
   return (
-    <div className="fixed right-5 bottom-5">
+    <div className="z-50 fixed right-5 bottom-5">
       <div
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         className="w-[60px] h-[60px] cursor-pointer"
--- src/components/feedback/AllFeedbacks.tsx
+++ src/components/feedback/AllFeedbacks.tsx
@@ -1,12 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import LoadingUI from "@/components/UI/my-loading/LoadingUI";
 import FeedbackItself from "@/components/feedback/FeedbackItself";
 import { contextData } from "@/components/context/context";
 import { FeedbacksTypes } from "@/components/types/types";
+import GoUpButton from "@/components/UI/my-buttons/go-up-button/GoUpButton";
 
 const AllFeedbacks = () => {
-  const { mainLanguage, feedbacks, loadedFeedbacks } = useContext(contextData);
+  const { mainLanguage, feedbacks, loadedFeedbacks, isVisible, setIsVisible } =
+    useContext(contextData);
 
+  useEffect(() => {
+    window.addEventListener("scroll", () =>
+      setIsVisible(window.scrollY > 1000),
+    );
+  }, []);
   return (
     <div className="mt-8 flex flex-col items-center justify-center">
       <p className="text-center text-3xl">
@@ -22,6 +29,7 @@ const AllFeedbacks = () => {
         ) : (
           <LoadingUI />
         )}
+        {isVisible && <GoUpButton />}
       </div>
     </div>
   );
