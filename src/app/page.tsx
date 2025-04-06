4c6e4a0a1058bce959217f57bb72c4d4bf5c2d72
akzhol0 <ak.akzhol1603@gmail.com>
2025-04-06 06:34:18 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,8 +30,8 @@ function Main() {
         />
         <div className="flex justify-center items-center">
           <Image
-            priority={true}
             src="/images/protection.png"
+            priority={true}
             width={500}
             height={500}
             alt="image"
