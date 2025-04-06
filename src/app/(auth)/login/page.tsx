6a9610447f53fe52a3a8b7588a9f606e73fd1576
akzhol0 <ak.akzhol1603@gmail.com>
2025-04-06 06:51:19 +0500

    new

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
-import LoginComponent from '@/components/auth/LoginComponent';
-import { Metadata } from 'next';
-import React from 'react';
+import LoginComponent from "@/components/auth/LoginComponent";
+import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
-  title: 'Войти в аккаунт',
+  title: "Войти в аккаунт",
 };
 
 function Login() {
