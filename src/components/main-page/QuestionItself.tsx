da7c02416768c7be57a83118cca43b4140ab3929
akzhol0 <ak.akzhol1603@gmail.com>
2025-04-18 16:07:15 +0500

    debug1231

--- src/components/main-page/QuestionItself.tsx
+++ src/components/main-page/QuestionItself.tsx
@@ -18,7 +18,7 @@ const QuestionItself = ({ item }: QuestionItselfProps) => {
       className="w-full flex justify-between items-center lg:py-0 min-h-[60px] cursor-pointer hover:bg-[#1b1b1b] transition-[.2s] rounded-lg md:ps-4"
     >
       <div
-        className={`relative w-full flex flex-col gap-2 px-2 md:px-8 py-4 transition-all duration-200 ease-in-out overflow-hidden
+        className={`relative w-full justify-between flex flex-col gap-2 px-2 md:px-8 py-4 transition-all duration-200 ease-in-out overflow-hidden
         ${isSelected ? "min-h-[140px] md:min-h-[160px] lg:min-h-[120px] sm:min-h-[120px]" : "min-h-[0px]"}`}
       >
         <div className="w-full flex justify-between text-sm md:text-lg z-50">
