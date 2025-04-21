19a4b379eb0e48282057e665f33a1c5b2a0ffa2c
akzhol0 <ak.akzhol1603@gmail.com>
2025-04-21 17:40:56 +0500

    debug z

--- src/components/main-page/QuestionItself.tsx
+++ src/components/main-page/QuestionItself.tsx
@@ -21,7 +21,7 @@ const QuestionItself = ({ item }: QuestionItselfProps) => {
         className={`relative w-full justify-between flex flex-col gap-2 px-2 md:px-8 py-4 transition-all duration-200 ease-in-out overflow-hidden
         ${isSelected ? "min-h-[140px] md:min-h-[160px] lg:min-h-[120px] sm:min-h-[120px]" : "min-h-[0px]"}`}
       >
-        <div className="w-full flex justify-between text-md md:text-lg z-50">
+        <div className="w-full flex justify-between text-md md:text-lg">
           <p>
             {item.id}. {item.question}
           </p>
@@ -32,7 +32,7 @@ const QuestionItself = ({ item }: QuestionItselfProps) => {
           </div>
         </div>
         <div
-          className={`absolute text-sm md:text-lg bottom-0 z-1 md:bottom-4 duration-500 ${isSelected ? "opacity-100" : "opacity-0"}`}
+          className={`absolute text-sm md:text-lg bottom-0 md:bottom-4 duration-500 ${isSelected ? "opacity-100" : "opacity-0"}`}
         >
           {mainLanguage.rest.answer}: {item.answer}
         </div>
