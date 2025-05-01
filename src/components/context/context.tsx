321776683ed8b6e51ecd6edb00ffd6803b1ccc86
akzhol0 <ak.akzhol1603@gmail.com>
2025-05-01 19:34:31 +0500

    update

--- src/components/context/context.tsx
+++ src/components/context/context.tsx
@@ -124,7 +124,7 @@ export function ContextOverAll({ children }: ContextOverAllProps) {
         id: doc.id,
         ...doc.data(),
       }));
-
+      console.log("working");
       setAllUsersMessages(messages); // Overwrite the full state with new data
     });
   };
