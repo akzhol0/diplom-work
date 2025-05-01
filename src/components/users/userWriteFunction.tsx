9765d07b470eaf8a7a785626e4af5679c6b2795c
akzhol0 <ak.akzhol1603@gmail.com>
2025-05-01 19:45:56 +0500

    updated

--- src/components/users/userWriteFunction.tsx
+++ src/components/users/userWriteFunction.tsx
@@ -24,30 +24,29 @@ const UserWriteFunction = ({
 }: UserWriteFunctionProps) => {
   const [userInputMessage, setUserInputMessage] = useState("");
 
-  const [localMessages, setLocalMessages] = useState<any>([]);
+  // const [localMessages, setLocalMessages] = useState<any>([]);
+  //
+  // const getAllUserMessages = async () => {
+  //   const docRef = doc(db, "userMessages", id);
+  //
+  //   onSnapshot(docRef, (docSnap) => {
+  //     if (docSnap.exists()) {
+  //       const docData = docSnap.data();
+  //       const dataArray = Object.entries(docData).map(([key, value]) => ({
+  //         key,
+  //         value,
+  //       }));
+  //
+  //       setLocalMessages(dataArray);
+  //     } else {
+  //       console.log(123);
+  //     }
+  //   });
+  // };
 
-  const getAllUserMessages = async () => {
-    const docRef = doc(db, "userMessages", id);
-
-    onSnapshot(docRef, (docSnap) => {
-      if (docSnap.exists()) {
-        const docData = docSnap.data();
-        const dataArray = Object.entries(docData).map(([key, value]) => ({
-          key,
-          value,
-        }));
-
-        setLocalMessages(dataArray);
-        console.log(dataArray);
-      } else {
-        console.log(123);
-      }
-    });
-  };
-
-  useEffect(() => {
-    getAllUserMessages();
-  }, []);
+  // useEffect(() => {
+  //   getAllUserMessages();
+  // }, []);
 
   const handleSubmitSendMessage = async () => {
     if (userInputMessage === "") return;
@@ -89,23 +88,27 @@ const UserWriteFunction = ({
         </div>
         <div className="flex flex-col h-[500px] overflow-y-auto gap-2 my-4">
           {twoUserMessages.length !== 0 ? (
-            localMessages.map((item: any, index: number) => (
-              <div
-                className={`p-2 bg-gray-100 rounded-lg ${item.value.sendingUserId !== sendingUser.userId ? "self-start" : "self-end"} `}
-                key={index}
-              >
-                <div>
+            twoUserMessages
+              .sort((a: any, b: any) => {
+                a - b;
+              })
+              .map((item: any, index: number) => (
+                <div
+                  className={`p-2 bg-gray-100 rounded-lg ${item.sendingUserId !== sendingUser.userId ? "self-start" : "self-end"} `}
+                  key={index}
+                >
                   <div>
-                    {item.value.sendingUserId === sendingUser.userId ? (
-                      <p>{sendingUser.userName}:</p>
-                    ) : (
-                      <p>{receivingUser.userName}:</p>
-                    )}
+                    <div>
+                      {item.sendingUserId === sendingUser.userId ? (
+                        <p>{sendingUser.userName}:</p>
+                      ) : (
+                        <p>{receivingUser.userName}:</p>
+                      )}
+                    </div>
+                    <p>{item.message}</p>
                   </div>
-                  <p>{item.value.message}</p>
                 </div>
-              </div>
-            ))
+              ))
           ) : (
             <div className="h-full text-lg flex justify-center items-center">
               <p>Пусто</p>
