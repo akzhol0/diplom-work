1e0e58756e1607a91d283bb7c31f95ed5523b34c
akzhol0 <ak.akzhol1603@gmail.com>
2025-05-03 21:31:44 +0500

    Updated communic function ended

--- src/components/users/UserPage.tsx
+++ src/components/users/UserPage.tsx
@@ -106,11 +106,7 @@ const UserPage = ({ token }: UserPageProps) => {
           <div className="flex flex-col">
             <div className="flex flex-col md:flex-row gap-4 items-center ">
               <Image
-                src={
-                  user?.gender === "Женщина"
-                    ? "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3o5Z3g3OHRlN210bzBocHhqdHV0MXBibHJqdjlndGh3NzI5dmY4YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L9fiOLupBnbMiKbXdJ/giphy.gif"
-                    : "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3RocXA3bGEwZXpuZzh5b3Y3aDdjY3NvaTlkZTFoczY2OWxxMHF3byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/F5YTPX0RukZe4KYTGS/giphy.gif"
-                }
+                src={user.image}
                 alt="user-png"
                 width={180}
                 height={180}
