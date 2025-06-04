58c8671caeb8f85d21b5c40cc4e104b5e6530a65
akzhol0 <ak.akzhol1603@gmail.com>
2025-06-04 17:17:30 +0500

    debug and added button check

--- src/components/main-page/Parallax.tsx
+++ src/components/main-page/Parallax.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import MyDangerButton from "@/components/UI/my-buttons/MyDangerButton";
+import MyPrimaryButton from "@/components/UI/my-buttons/MyPrimaryButton";
 
 type ParallaxProps = {
   title: string;
@@ -13,7 +16,12 @@ function Parallax({ title, small }: ParallaxProps) {
         {title}
       </p>
       <div className="max-w-[600px] text-[18px] md:text-[20px]">
-        <p>{small}</p>
+        <p className="mb-2">{small}</p>
+        <Link href="/services">
+          <MyPrimaryButton className="text-sm px-4 py-2">
+            Попробовать сейчас
+          </MyPrimaryButton>
+        </Link>
         <div className="my-4">
           <Image
             src="https://cloud.githubusercontent.com/assets/835857/14581711/ba623018-0436-11e6-8fce-d2ccd4d379c9.gif"
