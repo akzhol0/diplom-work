ed9c56a1e236cf583e7f956a60f73cd653c6b717
akzhol0 <ak.akzhol1603@gmail.com>
2025-07-10 21:40:46 +0500

    added

---
+++ src/app/test/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "010hn6z3t5fq5sjo",
+};
+
+function Services() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <p className="font-semibold text-3xl">010hn6z3t5fq5sjo</p>
+    </div>
+  );
+}
+
+export default Services;
