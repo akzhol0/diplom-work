58904753a14686125cb32e143b47761ba5793134
akzhol0 <ak.akzhol1603@gmail.com>
2025-12-30 14:52:58 +0500

    debug

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("start");
+await new Promise((resolve) => setTimeout(resolve, 3000));
+console.log("timeout / check finished");
+console.log("tests completed");
+// aint no way, nonе of the bugs out there getting over that 😎
