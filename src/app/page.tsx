9626d7893a3f96fca87d98354fb5e8593e63baef
akzhol0 <ak.akzhol1603@gmail.com>
2025-12-30 13:31:22 +0500

    na 4to 9 tra4y svoe svobodnoe vrem9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,15 @@ function Main() {
   }, []);
 
   return (
-    <div className="w-full flex flex-col items-center bg-[#ffffff]">
-      <div className="w-[95%] md:w-[80%] flex flex-col lg:flex-row justify-around">
+    <div className="flex w-full flex-col items-center bg-[#ffffff]">
+      <div className="flex w-[95%] flex-col justify-around md:w-[80%] lg:flex-row">
         <Parallax
           small={mainLanguage.mainPage.parallax.small}
           title={mainLanguage.mainPage.parallax.title}
           tryNow={mainLanguage.leftOut.tryNow}
         />
-        <div className="flex justify-center items-center">
-          <div className="rounded-xl overflow-hidden mb-4">
+        <div className="flex items-center justify-center">
+          <div className="mb-4 overflow-hidden rounded-xl">
             <Image
               src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXFvaWprYmwwdXZ0NXhxanNpeTk1NWtkZGZkOGljZ2llcDZybDQ2dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RDZo7znAdn2u7sAcWH/giphy.gif"
               priority={true}
@@ -46,8 +46,8 @@ function Main() {
           </div>
         </div>
       </div>
-      <div className="w-full min-h-[400px] bg-[#f8f8f8] flex justify-center items-center">
-        <div className="w-[95%] md:w-[80%] gap-4 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 ">
+      <div className="flex min-h-[400px] w-full items-center justify-center bg-[#f8f8f8]">
+        <div className="grid w-[95%] grid-cols-1 gap-4 sm:grid-cols-2 md:w-[80%] xl:grid-cols-4">
           {mainLanguage.mainPage.cards.map((item: any) => (
             <CardComp key={item.id} item={item} />
           ))}
@@ -59,11 +59,18 @@ function Main() {
         </video>
       </div>
       <ServicesMainPage />
-      <p className="text-[20px] md:text-[30px] text-center text-black font-semibold mt-[20px] md:mt-[60px]">
+      <p className="mt-[20px] text-center text-[20px] font-semibold text-black md:mt-[60px] md:text-[30px]">
         {mainLanguage.mainPage.questionLabel}
       </p>
-      <div className="w-full flex justify-center bg-[#131313]">
+      <div className="flex w-full justify-center bg-[#131313]">
         <Qa />
+      </div>
+      <div className="m-4 p-2 bg-gray-100 rounded-lg text-md md:text-lg">
+        <div className="ps-4 flex items-center">
+          Прикинь, в твоей жизни произошло столько событий, столько всего должно
+          было произойти, чтобы ты именно сейчас читал это сообщение, ну что ты
+          здесь забыл?
+        </div>
       </div>
       <Information
         title={mainLanguage.mainPage.title3}
@@ -76,15 +83,16 @@ function Main() {
       <video width="900" height="600" playsInline autoPlay loop muted>
         <source src="/videos/video2.mp4" type="video/mp4" />
       </video>
-      <div className="w-full flex flex-col items-center my-8">
+      <div className="my-8 flex w-full flex-col items-center">
         <UsedTech />
       </div>
       <div className="w-full sm:w-[90%] lg:w-[80%]">
         <FeedbacksMainPage />
       </div>
-      {/*<div className="w-full flex justify-center mt-[40px]">*/}
-      {/*  <AboutDeveloper />*/}
-      {/*</div>*/}
+      <div className="w-full flex justify-center mt-[40px]">
+        <AboutDeveloper />
+      </div>
+
       {isVisible && <GoUpButton />}
       <ChatBot />
       <UsersListModal />
